7458fe9fa71005bd8db2854135e93136c0c3223a
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-04 03:17:21 +0600

    first commit

---
+++ src/productsContent.js
@@ -0,0 +1,180 @@
+export const products = [
+  {
+    id: 1,
+    title: "MacBook Pro M4",
+    price: 2399.99,
+    category: "Laptops",
+    description: "Sleek, powerful, and ideal for creators.",
+    image:
+      "https://www.notebookcheck.net/fileadmin/_processed_/webp/Notebooks/Apple/MacBook_Pro_14_2024_M4/IMG_7747-JPG-q82-w2560-h.webp",
+  },
+  {
+    id: 2,
+    title: "Dell XPS 13 OLED",
+    price: 1899.99,
+    category: "Laptops",
+    description: "Sharp OLED display with high portability.",
+    image:
+      "https://c.pxhere.com/photos/42/d6/Beats_CC0_Computer_Device_Headphones_iPad_Keyboard_Laptop-1610710.jpg!d",
+  },
+  {
+    id: 3,
+    title: "Samsung Galaxy S25",
+    price: 1299.99,
+    category: "Smartphones",
+    description: "Flagship-level camera and performance.",
+    image:
+      "https://img.us.news.samsung.com/us/wp-content/uploads/2025/01/22132755/%EA%B0%A4%EB%9F%AD%EC%8B%9C_S25_%EC%8B%9C%EB%A6%AC%EC%A6%88-950x633.jpg",
+  },
+  {
+    id: 4,
+    title: "iPad Pro M4",
+    price: 999.99,
+    category: "Tablets",
+    description: "Top-tier screen and performance in a sleek tablet.",
+    image:
+      "https://i.guim.co.uk/img/media/2b2abbe3c3e4ee9c4b80a88d7fae5e905e77896f/0_229_5456_3273/master/5456.jpg?width=1900&dpr=2&s=none&crop=none",
+  },
+  {
+    id: 5,
+    title: "Asus ROG Zephyrus G14",
+    price: 1799.99,
+    category: "Laptops",
+    description: "Gaming-grade laptop in a compact form.",
+    image: "https://m.media-amazon.com/images/I/71+DAcqLZmL._AC_SL1500_.jpg",
+  },
+  {
+    id: 6,
+    title: "Sony WH-1000XM6",
+    price: 349.99,
+    category: "Headphones",
+    description: "Industry-leading noise cancellation and sound.",
+    image: "https://cdn.mos.cms.futurecdn.net/49YqdsTeHeJZJQWJRSYMef.jpg",
+  },
+  {
+    id: 7,
+    title: "Apple Watch Series 10",
+    price: 499.99,
+    category: "Smart Watches",
+    description: "Next-gen watch with all-day health tracking.",
+    image:
+      "https://assets.gadgetandgear.com/upload/media/apple-watch-10--46mm-rose-gold.jpeg",
+  },
+  {
+    id: 8,
+    title: "Bose SoundLink Revolve+ II",
+    price: 279.99,
+    category: "Speakers",
+    description: "360° sound in a portable package.",
+    image:
+      "https://www.gadgetstyler.com/wp-content/uploads/2023/04/71wtzn5AnL._AC_SL1500_.jpg",
+  },
+  {
+    id: 9,
+    title: "LG UltraFine 5K Monitor",
+    price: 1299.99,
+    category: "Monitors",
+    description: "Stunning 5K resolution for professionals.",
+    image:
+      "https://www.lgnewsroom.com/wp-content/uploads/2016/10/LG-UltraFine-5K-Monitor-scaled.jpg",
+  },
+  {
+    id: 10,
+    title: "Logitech MX Keys S",
+    price: 99.99,
+    category: "Keyboards",
+    description: "Precision typing with smart illumination.",
+    image:
+      "https://www.pcworld.com/wp-content/uploads/2025/04/DSC03106.jpg?quality=50&strip=all",
+  },
+  {
+    id: 11,
+    title: "Razer DeathAdder V3",
+    price: 59.99,
+    category: "Mice",
+    description: "High-performance mouse for esports-quality tracking.",
+    image:
+      "https://i.rtings.com/assets/products/XZTWfCaG/razer-deathadder-v3-pro/design-large.jpg?format=auto",
+  },
+  {
+    id: 12,
+    title: "Surface Pro 10",
+    price: 1299.99,
+    category: "Tablets",
+    description: "Versatile 2‑in‑1 laptop-tablet hybrid.",
+    image:
+      "https://static1.xdaimages.com/wordpress/wp-content/uploads/2024/03/surface-pro-10-for-business_no-copilot-in-windows-with-background.jpg?q=50&fit=crop&w=1100&h=618&dpr=1.5",
+  },
+  {
+    id: 13,
+    title: "Lenovo Legion 7i",
+    price: 2099.99,
+    category: "Laptops",
+    description: "Full-power gaming desktop replacement.",
+    image:
+      "https://p1-ofp.static.pub//fes/cms/2024/09/12/q6fb2891avf5ok5et6ppuhuuilu0cq939626.png",
+  },
+  {
+    id: 14,
+    title: "Google Pixel 9 Pro",
+    price: 899.99,
+    category: "Smartphones",
+    description: "AI-smartphone with clean Android and great camera.",
+    image:
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202408/pixel-9-pro-xl--pixel-9-213429575-1x1.jpg?VersionId=QcO8h1OLB05Z.JUNyK.JODhq4Y0mBdJ9",
+  },
+  {
+    id: 15,
+    title: "OnePlus Pad 2",
+    price: 749.99,
+    category: "Tablets",
+    description: "Fast charging, crisp display, sleek design.",
+    image:
+      "https://www.oneplus.com/content/dam/oasis/page/2024/global/product/rainbow/rainbow-share.jpg",
+  },
+  {
+    id: 16,
+    title: "HyperX Cloud III",
+    price: 119.99,
+    category: "Headphones",
+    description: "Comfortable headset built for long gaming sessions.",
+    image:
+      "https://cdn.prod.website-files.com/64adca1358d4f63115d85133/64adcb69b1c9d8527f4a1c35_6488b105919fcffd04f673af_HyperX%2520Cloud%2520III%2520Gaming%2520Headset%2520KF%25208%2520Square.jpeg",
+  },
+  {
+    id: 17,
+    title: "JBL Flip 7",
+    price: 149.99,
+    category: "Speakers",
+    description: "Portable, waterproof Bluetooth speaker.",
+    image:
+      "https://www.cnet.com/a/img/resize/ea8ba3d48aff93bcd9c8c6da1edd025ca6757deb/hub/2025/03/10/b42421ba-1ef8-4351-9aa2-2483003cfe6f/jbl-flip-7-and-charge-6-together-1.jpg?auto=webp&fit=crop&height=1200&width=1200",
+  },
+  {
+    id: 18,
+    title: "Samsung Smart Monitor M8",
+    price: 399.99,
+    category: "Monitors",
+    description: "Smart display with streaming & productivity features.",
+    image:
+      "https://www.pcworld.com/wp-content/uploads/2025/04/samsung-smart-monitor-m8-2.jpg?quality=50&strip=all",
+  },
+  {
+    id: 19,
+    title: "Keychron K8 Pro",
+    price: 129.99,
+    category: "Keyboards",
+    description: "Solid wireless mechanical keyboard with RGB lighting.",
+    image:
+      "https://siliconz.vn/cdn/shop/files/9b0ef55e5372beef159a8f4ae0db03f7.png?v=1728187488&width=1946",
+  },
+  {
+    id: 20,
+    title: "Logitech MX Master 4",
+    price: 99.99,
+    category: "Mice",
+    description: "Ergonomic, high‑precision multi-device mouse.",
+    image:
+      "https://9to5mac.com/wp-content/uploads/sites/6/2025/06/mx-master4.jpg?quality=82&strip=all",
+  },
+];
