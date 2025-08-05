81bb9de6d55f3836180172fc505cf4065f8c8df9
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-05 22:57:22 +0600

    Cart redux-toolkit setup

--- src/App/Store.js
+++ src/App/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import ProductReducer from "../features/products/productSlice";
+import CartReducer from "../features/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
     product: ProductReducer,
+    cart: CartReducer,
   },
 });
